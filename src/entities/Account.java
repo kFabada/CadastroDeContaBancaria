2ee74a39eae41786d2783fc491609e60222274dc
Fabiano Silva <FabianoSantos2004@outlook.com.br>
2024-01-21 15:15:48 -0300

    Finalização do Projeto de Cadastro

---
+++ src/entities/Account.java
@@ -0,0 +1,15 @@
+public class Cliente {
+
+    private int number;
+    private String holder;
+    public double deposit;
+
+    public Cliente(){}
+    public Cliente(int number, String holder, double deposit) {
+        this.number = number;
+        this.holder = holder;
+        this.deposit = deposit;
+    }
+
+
+}
